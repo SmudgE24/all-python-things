930c4ae0cf53f91b313a6a8dbd6a13a47c3d9ad4
SmudgE24 <167187731+SmudgE24@users.noreply.github.com>
2024-12-12 15:39:40 +0000

    Add files via upload

---
+++ christmas countdown 2.py
@@ -0,0 +1,99 @@
+import datetime as dt
+import time as t
+
+#dt.timedelta.total_seconds
+#
+#sound_file = '/home/pi/Downloads/mixkit-classic-alarm-995.wav'
+#
+#wave_obj = sa.WaveObject.from_wave_file(sound_file)
+#play_obj = wave_obj.play()
+#play_obj.wait_done()
+n1 = 1
+in_one = 0
+in_one = input('1 = Cristmas, 2 = Ethan, 3 = year-seven')
+#in_one = input
+
+
+def christmas():
+    christmas = dt.datetime(2024, 12, 25, 7, 30, 0)
+    now = dt.datetime.now()
+    diff = dt.timedelta.total_seconds(christmas - now)
+    
+    while diff>0:
+        now = dt.datetime.now()
+        diff = dt.timedelta.total_seconds(christmas - now)
+        print (round(diff, 1),' seconds \'til Christmas')
+        #t.sleep(0.1)
+        #play_obj = wave_obj.play()
+        
+    if diff == 0:
+        print ('IT\'S CHRISTMAS!!!!!!!!!!')
+        while n1 == 1:
+            print ('IT\'S CHRISTMAS!!!!!!!!!!')
+
+def ethanbirthday():
+    ethan = dt.datetime(2024, 3, 24, 7, 30, 0)
+    now = dt.datetime.now()
+    diff = dt.timedelta.total_seconds(ethan - now)
+        
+    while diff>0:
+        now = dt.datetime.now()
+        diff = dt.timedelta.total_seconds(ethan - now)
+        print (round(diff, 1),' seconds \'til Ethans\'s birthday')
+        #t.sleep(0.01)
+        #play_obj = wave_obj.play()
+        
+    if diff == 0:
+        print ('IT\'S ETHAN\'S BIRTHDAY!!!!!!!!!!')
+        while n1 == 1:
+            print ('IT\'S ETHAN\'S BIRTHDAY!!!!!!!!!!')
+
+def schoolstart():
+    school = dt.datetime(2025, 9, 4, 8, 0, 0)
+    now = dt.datetime.now()
+    diff = dt.timedelta.total_seconds(school - now)
+        
+    while diff>0:
+        now = dt.datetime.now()
+        diff = dt.timedelta.total_seconds(school - now)
+        rounded_diff = round(diff, 1)
+        print (rounded_diff,' seconds \'til school starts')
+        #t.sleep(0.1)
+        #play_obj = wave_obj.play()
+        
+    if diff == 0:
+        print ('IT\'S SCHOOL NOW!!!!!!!!!!')
+        while n1 == 1:
+            print ('IT\'S SCHOOL NOW!!!!!!!!!!')
+
+if (int(in_one)) == 1:
+    christmas()
+if (int(in_one)) == 2:
+    ethanbirthday()
+if (int(in_one)) == 3:
+    schoolstart()
+
+
+#mixkit-classic-alarm-995.wav
+#/home/pi/Downloads
+    
+    
+    
+
+
+
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
+print ('')
---
+++ messagedecoder.py
@@ -0,0 +1,48 @@
+import random 
+listofletters = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+alphebet = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+
+def randomNumbers(begining_seed, listofletters):
+    scrambledletters = []
+    amountoftries = 52
+    random.seed(begining_seed)
+    for i in range(53):
+        #random.seed(begining_seed)
+        random_number = (random.randint(0, amountoftries))
+        letter = (listofletters[random_number])
+        listofletters.remove(letter)
+        scrambledletters.append(letter)
+        amountoftries = amountoftries - 1
+    return scrambledletters
+
+
+def indexFinder (message, scrambledletters, i):
+    #find the letter in the message
+    letter = message[i]
+    #find the index of the letter in the alphebet
+    index = scrambledletters.index(letter)
+    return index
+
+
+
+begining_seed = input("what beginning seed do you want?")
+
+message = input("what message do you want to decode?")
+
+scrambledletters = randomNumbers(begining_seed, listofletters)
+outputlist = []
+
+i = 0
+while i in range (len(message)):
+    findwhere = indexFinder (message, scrambledletters, i)
+    outputletter = (alphebet[findwhere])
+    outputlist.append(outputletter)
+    i = i + 1
+
+
+
+
+joinything = ""
+print(joinything.join(outputlist))
